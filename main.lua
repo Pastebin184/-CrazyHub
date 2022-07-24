11e18ef33ec84c93972d7e181c6fac82cab6dac4
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2022-07-23 17:43:02 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
+
 local a = game.PlaceId
 if a == 8571687919 then
 	print("Game supported")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastebin184/-CrazyHub/main/unofficial.lua"))()
-else if a == 1 then
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/-CrazyHub/main/unofficial.lua')))()
+
+if a == 1 then
 	print("game supported")
 else 
     game.Players.LocalPlayer:Kick("Game not supported")

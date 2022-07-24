df593aa4c818e0c4a924d2e3aae7cd0fc6e060d5
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2022-07-24 16:11:52 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
+--Uno: 8571687919
+--Super: 2202352383
 
-local a = game.PlaceId
-if a == 8571687919 then
-	print("Game supported")
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/-CrazyHub/main/unofficial.lua')))()
+local Rplace = game.PlaceId
 
-if a == 1 then
-	print("game supported")
+if Rplace == 8571687919 then
+    print("Uno")
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/-CrazyHub/main/unofficial.lua')))()
+elseif Rplace == 2202352383 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/-CrazyHub/main/superpower.lua')))()
+    print("Super")
 else 
-    game.Players.LocalPlayer:Kick("Game not supported")
-end
+    print("nope")
 end
